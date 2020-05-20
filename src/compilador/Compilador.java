871416be196c1a02f0926369762b77b552d02cae
wilson <wilsonpuac@gmail.com>
2020-05-19 18:14:54 -0600

    correcciones

--- src/compilador/Compilador.java
+++ src/compilador/Compilador.java
@@ -10,8 +10,14 @@ public class Compilador {
 //        System.out.println("The type of the object is: " + cls.getName());
         File file = new File("C:\\proyecto_compiladores\\codigo.txt");
         Controller control =  new Controller();
-        control.obtenerDatos(file);
-        control.declaraciones();
+        try {
+            control.obtenerDatos(file);
+            control.declaraciones();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        
 
     }
     
